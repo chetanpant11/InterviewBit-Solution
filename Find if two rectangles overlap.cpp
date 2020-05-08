cb64aac08505ee580e7883d5b4f277380352b44d
Chetan Pant <43753655+chetanpant11@users.noreply.github.com>
2020-05-08 23:10:42 +0530

    Create Find if two rectangles overlap.cpp

---
+++ Find if two rectangles overlap.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solve(int a,int b,int c,int d,int e,int f,int g,int h)
+{
+    if(a>=g||e>=c)
+        return 0;
+    if(f>=d}}b>=h)
+        return 0;
+    return 1;
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b,c,d,e,f,g,h;
+    cin>>a>>b>>c>>d>>e>>f>>g>>h;
+    int ans=solve(a,b,c,d,e,f,g,h);
+    cout<<ans;
+}
